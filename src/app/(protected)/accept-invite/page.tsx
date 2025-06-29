c77b7b66e79f34339d0602d8303a448d1daf7271
Sumbal Hashmi <sumbalhashmi14@gmail.com>
2025-06-29 17:52:38 +0500

    fix

--- src/app/(protected)/accept-invite/page.tsx
+++ src/app/(protected)/accept-invite/page.tsx
@@ -1,33 +1,15 @@
-"use client"
-
 import AcceptInvite from '@/components/organisms/AcceptInvitation/AcceptInvitation'
-import { useSearchParams } from 'next/navigation'
-import { Suspense } from 'react'
 
-function AcceptInviteContent() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token') || ''
+export default function Page({ searchParams }: { 
+  searchParams: { token?: string } 
+}) {
+  const token = searchParams.token || ''
 
+   console.log(token);
+   
   return (
     <div className="h-screen bg-background flex items-center justify-center p-4">
       <AcceptInvite inviteToken={token}/>
     </div>
   )
 }
-
-function Page() {
-  return (
-    <Suspense fallback={
-      <div className="h-screen bg-background flex items-center justify-center p-4">
-        <div className="text-center">
-          <h1 className="text-2xl font-semibold mb-4">Loading...</h1>
-          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900 mx-auto"></div>
-        </div>
-      </div>
-    }>
-      <AcceptInviteContent />
-    </Suspense>
-  )
-}
-
-export default Page
